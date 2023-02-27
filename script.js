268b3f0b778bfdc9d4de7d4f105609b8a5091a37
SaerElMasri <saer1890@gmail.com>
2023-02-28 01:01:36 +0200

    Added functions to get a random category key and inside that key a random value, added event to disable the button after being clicked

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ alphabet_arr.forEach(letter => {
     letter_container.appendChild(span);
 });
 
+let category_span = document.getElementById("category");
+
 const words_disctionary = {
     fruits: [
       "Apple",
@@ -38,8 +40,7 @@ const words_disctionary = {
         "Spiderman",
         "Ironman",
         "Hulk",
-        "Thor",
-        "Capitan America",
+        "Thor"
     ],
     programming: [
         "Python",
@@ -49,9 +50,22 @@ const words_disctionary = {
         "C++",
     ],
 }
-
 let all_keys = Object.keys(words_disctionary);
 let random_key_index = Math.floor(Math.random() * all_keys.length);
 let randome_key_name = all_keys[random_key_index];
 let key_values = words_disctionary[randome_key_name];
 let random_value_index = Math.floor(Math.random() * key_values.length);
+let random_value = key_values[random_value_index];
+category_span.innerHTML = randome_key_name;
+
+let guess_container = document.querySelector(".guesses");
+let letter_space = Array.from(random_value).forEach(letter => {
+    let span = document.createElement("span");
+    guess_container.appendChild(span);
+});
+
+document.addEventListener("click", function(e){
+    if(e.target.className == "letter-btn"){
+        e.target.classList.add("clicked");
+    }
+})
