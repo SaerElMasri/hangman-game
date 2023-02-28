336c435ac7b45061f68128bdf2f5348c57dcb2ab
SaerElMasri <saer1890@gmail.com>
2023-02-28 02:44:20 +0200

    Fixed hangman parts and cleaned the code

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-const alphabet = "abcdefghijklmnopqrstuvwxyz";
-let alphabet_arr = Array.from(alphabet);
-let letter_container = document.querySelector(".letters");
-alphabet_arr.forEach(letter => {
-    let span = document.createElement("span");
-    let letter_node = document.createTextNode(letter);
-    span.appendChild(letter_node);
-    span.className = 'letter-btn';
-    letter_container.appendChild(span);
-});
-
-let category_span = document.getElementById("category");
-
 const words_disctionary = {
     fruits: [
       "Apple",
@@ -50,6 +37,17 @@ const words_disctionary = {
         "C++",
     ],
 }
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+let alphabet_arr = Array.from(alphabet);
+let letter_container = document.querySelector(".letters");
+alphabet_arr.forEach(letter => {
+    let span = document.createElement("span");
+    let letter_node = document.createTextNode(letter);
+    span.appendChild(letter_node);
+    span.className = 'letter-btn';
+    letter_container.appendChild(span);
+});
+let category_span = document.getElementById("category");
 let all_keys = Object.keys(words_disctionary);
 let random_key_index = Math.floor(Math.random() * all_keys.length);
 let randome_key_name = all_keys[random_key_index];
@@ -57,14 +55,12 @@ let key_values = words_disctionary[randome_key_name];
 let random_value_index = Math.floor(Math.random() * key_values.length);
 let random_value = key_values[random_value_index];
 category_span.innerHTML = randome_key_name;
-
 let guess_container = document.querySelector(".guesses");
 let letter_space = Array.from(random_value);
 letter_space.forEach(letter => {
     let span = document.createElement("span");
     guess_container.appendChild(span);
 });
-
 let letter_guess = document.querySelectorAll(".guesses span");
 let wrong_counter = 0;
 let draw = document.querySelector(".man-draw");
